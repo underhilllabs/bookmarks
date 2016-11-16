170d1a6c6829b2e7895f599b93831cc9a7129a43
Bart Lantz <underhilllabs@gmail.com>
2016-11-15 22:09:59 -0700

    adds auth plug

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Bookmarks.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Bookmarks.Auth, repo: Bookmarks.Repo
   end
 
   pipeline :api do
