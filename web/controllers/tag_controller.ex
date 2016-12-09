94550434f65ea4d0ad698bfcf284a985d9e040d5
Bart Lantz <underhilllabs@gmail.com>
2016-12-09 16:18:14 -0700

    updates tag controller to work with many_to_many relations

--- web/controllers/tag_controller.ex
+++ web/controllers/tag_controller.ex
@@ -2,13 +2,15 @@ defmodule Bookmarks.TagController do
   use Bookmarks.Web, :controller
 
   alias Bookmarks.Tag
+  alias Bookmarks.BookmarkTag
   import Ecto.Query, only: [from: 2]
 
   def index(conn, _params) do
-    tags_count = from(t in Tag, 
-                      select: [t.name, count(t.name)], 
+    tags_count = from(t in Tag,
+                      join: bt in BookmarkTag, on: bt.tag_id == t.id,
+                      select: [t.name, count(bt.tag_id)],
                       group_by: t.name,
-                      order_by: [desc: count(t.name), asc: t.name]) 
+                      order_by: [desc: count(bt.tag_id), asc: bt.tag_id])
                  |> Repo.all
     render(conn, "index.html", tags_count: tags_count)
   end
@@ -16,12 +18,12 @@ defmodule Bookmarks.TagController do
   def name(conn, params) do
     %{"name" => name} = params
     page = from(b in Bookmarks.Bookmark,
-                    join: t in Tag, on: b.id == t.bookmark_id,
+                    join: bt in BookmarkTag, on: b.id == bt.bookmark_id,
+                    join: t in Tag, on: t.id == bt.tag_id,
                     where: t.name == ^name,
                     order_by: [desc: b.updated_at],
                     preload: [:tags, :user])
                 |> Repo.paginate(params) 
-    #render(conn, "name.html", name: name, bookmarks: bookmarks)
     render conn, "name.html", 
       name: name,
       bookmarks: page.entries,
