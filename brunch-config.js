1a42fbc0ef9094e08b3d1626417baa65f359b5de
Bart Lantz <underhilllabs@gmail.com>
2016-12-17 14:22:21 -0700

    updates js and css

--- brunch-config.js
+++ brunch-config.js
@@ -3,7 +3,6 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: "js/app.js"
-
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
       // joinTo: {
@@ -12,12 +11,12 @@ exports.config = {
       // }
       //
       // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "web/static/vendor/js/jquery-2.1.1.js",
-      //     "web/static/vendor/js/bootstrap.min.js"
-      //   ]
-      // }
+      order: {
+        before: [
+          //"web/static/vendor/js/jquery-3.1.1.min.js",
+          //"web/static/vendor/js/bootstrap.min.js"
+        ]
+      }
     },
     stylesheets: {
       joinTo: "css/app.css",
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,17 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import $ from "./vendor/jquery-3.1.1.min"
+//import $ from "./vendor/bootstrap.min"
+
+// Some bootstrap things need this, not most thankfully, keep disabled unless required, this one might require it?
+//global.jQuery = require("jquery")
+
+// Make sure you included bootstrap js in your npm and brunch config files or this will not work
+global.bootstrap = require("bootstrap")
+
+// Do the stuff to the existing html at page load time:
+$(document).ready(() => {
+    // This is optional if you want to do it on 'something other than default'
+      $('[data-toggle="collapse"]').collapse()
+}
